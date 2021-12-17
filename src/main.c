60e8c7e856bc8f9ac3db02af97223e015bdce841
in hee <91165073+inhee-lee-cs102@users.noreply.github.com>
2021-12-17 15:44:25 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,30 @@
-/*
- * main.c
- */
-#include "main.h"
+#include "calendar.h"
 
+//Main Function//
 int main(void) {
-    printf("Welcome to %s!\n",APP_NAME);
-    return(SUCCESS);
+  int code(void);
+  printf("\t Calender: Final C Project \n");
+  int count = 0;
+  printf("Input 1 to Enter Event and 2 to Recall Event. Enter 3 to End Program: \n");
+  int option;
+  scanf("%d", &option);
+  while (option != 3 && count < 20) {
+  if (option == 1){
+    inputevent(count);
+    count++; /* gives the input function when 1 is inputed */
+  }
+  else{
+  if (option == 2){
+     recallevent();/* gives the recall function when 2 is inputed */
+  }
+  else{
+    printf("Invalid Input \n");
+  }
+  }
+  printf("Input 1 to Enter Event and 2 to Recall Event. Enter 3 to End Program: \n");
+  scanf("%d", &option);
+  }
+
+
+  return 0;
 }
