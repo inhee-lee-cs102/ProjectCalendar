c408bd598881ced4c745489301f5e55548d77c24
in hee <91165073+inhee-lee-cs102@users.noreply.github.com>
2021-12-17 15:44:50 -0500

    Update calendar.h

--- src/calendar.h
+++ src/calendar.h
@@ -4,8 +4,19 @@
 #ifndef CALENDAR_H
 #define CALENDAR_H
 
-#include <stdio.h>
+#include "main.h"
 #include <string.h>
-#include <stdlib.h>
+/* declare the integers and chararacters needed for the program */
+int montharray[20];
+int dayarray[20];
+char namearray[20][20];
+int month;
+int day;
+int i;
+int month;
+int day;
+char name[20];
+
+
 
 #endif /* CALENDAR_H */
